b850c3dd58a6681f38290ee2ecd49a326e1697d1
Vinay Mundhe <vinay.mundhe19@gmail.com>
2024-09-13 21:19:02 +0530

    Initial commit

---
+++ src/main/java/com/va/vinassets/VinassetsApplication.java
@@ -0,0 +1,13 @@
+package com.va.vinassets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinassetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinassetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/va/vinassets/VinassetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.va.vinassets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinassetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
